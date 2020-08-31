a0a04d211e56e19fef74c3c0461d4106a406319f
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-08-31 15:23:19 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -143,7 +143,7 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Support Group", url="malayaliinfos"), InlineKeyboardButton(text="Support Bot", url="t.me/malayaliLinksbot)]]), 
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Support Group", url="t.me/malayaliinfos"), InlineKeyboardButton(text="Support Bot", url="t.me/malayaliLinksbot")]]), 
                 parse_mode=ParseMode.MARKDOWN)
     else:
         update.effective_message.reply_text("waked up😏😏😏")
